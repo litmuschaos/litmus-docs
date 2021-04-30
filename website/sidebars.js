39c6aed40478278bfbe7d9582a732a1480fd8324
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-05-01 01:44:22 +0530

    Minor changes added.
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -8,7 +8,7 @@ module.exports = {
     "Getting Started": [
       "getstarted",
       {
-	      "Installation" : [ "litmus-install", "agent-install", "litmus-with-ingress"],
+	      "Installation" : [ "litmus-install", "agent-install", "litmus-with-nginx"],
       },
       "create-workflow",
       "observe-workflow",
