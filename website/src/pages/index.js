6a6e7d63755cefc579483269598e0ac37dc3c06c
Amit Kumar Das <amit@chaosnative.com>
2021-05-25 12:04:23 +0530

    Added the basic layout for homepage
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,7 +1,38 @@
 import React from "react";
-import { Redirect } from "@docusaurus/router";
-function Home() {
-  return <Redirect to="/docs/" />;
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./styles.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
+
+function HomepageHeader() {
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx("hero hero--primary", styles.heroBanner)}>
+			<div className="container">
+				<h1 className="hero__title">{siteConfig.title}</h1>
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
+				<div className={styles.buttons}>
+					<Link className="button button--secondary button--lg" to="/docs/">
+						Litmus Docs
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
 
-export default Home;
+export default function Home() {
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<Layout
+			title={`Hello from ${siteConfig.title}`}
+			description="Description will go into a meta tag in <head />">
+			<HomepageHeader />
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	);
+}
