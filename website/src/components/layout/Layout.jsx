6ee89fd2eb12717494bcfde8dd950485066f395e
Sayan Mondal <sayan@chaosnative.com>
2021-06-18 14:43:34 +0530

    Adding Docusauras Layout
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- website/src/components/layout/Layout.jsx
+++ website/src/components/layout/Layout.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { theme } from '../../theme'
-// Components
-// import { Footer } from "../footer";
-import { Nav } from '../nav'
-import { PreFooter } from '../pre-footer'
 
-const Layout = ({ children }) => {
+const LitmusDocsThemeProvider = ({ children }) => {
 	return (
-		<ThemeProvider theme={theme()}>
-			<Nav />
-			<main>{children}</main>
-			<PreFooter />
+		<ThemeProvider theme={theme()}>			
+			<main>{children}</main>			
 		</ThemeProvider>
 	)
 }
 
-export { Layout }
+export { LitmusDocsThemeProvider }
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,29 +1,37 @@
-import React from 'react'
-import { Layout, SectionDark, SectionLight } from '../components/layout'
-import { ExploreLitmus, Header } from '../components/section/Home'
-import { GetStartedHeader } from '../components/section/Home/GetStartedHeader'
-import { MoreResources } from '../components/section/Home/MoreResources'
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import React from "react";
+import { LitmusDocsThemeProvider, SectionDark, SectionLight } from '../components/layout';
+import { ExploreLitmus, Header } from '../components/section/Home';
+import { GetStartedHeader } from '../components/section/Home/GetStartedHeader';
+import { MoreResources } from '../components/section/Home/MoreResources';
 
-const Home = () => {
+
+export default function Home() {
+	const { siteConfig } = useDocusaurusContext();
 	return (
-		<Layout>
-			{/* Pages Components */}
-			<div className="homeMainContent">
-				<SectionLight>
-					<Header />
-					<br />
-					<GetStartedHeader />
-					<br />
-					<ExploreLitmus type={'basic'} />					
-					<ExploreLitmus type={'advance'} />					
-					<br />
-				</SectionLight>
-				<SectionDark>
-					<MoreResources />
-				</SectionDark>
-			</div>
+		<Layout
+			title={`Hello from ${siteConfig.title}`}
+			description="Description will go into a meta tag in <head />">			
+			<main>
+				<LitmusDocsThemeProvider>
+				{/* Pages Components */}
+				<div className="homeMainContent">
+					<SectionLight>
+						<Header />
+						<br />
+						<GetStartedHeader />
+						<br />
+						<ExploreLitmus type={'basic'} />					
+						<ExploreLitmus type={'advance'} />					
+						<br />
+					</SectionLight>
+					<SectionDark>
+						<MoreResources />
+					</SectionDark>
+				</div>
+				</LitmusDocsThemeProvider>
+			</main>
 		</Layout>
-	)
+	);
 }
-
-export default Home
