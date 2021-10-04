971aae8e4bc6d17b1f17b4ded21034311e8cea4a
neelanjan00 <neelanjan@chaosnative.com>
2021-10-04 15:55:11 +0530

    disabled SectionDark
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- website/src/components/layout/Sections.jsx
+++ website/src/components/layout/Sections.jsx
@@ -1,20 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import '../../css/sections.css'
 
-import useThemeContext from '@theme/hooks/useThemeContext';
-
 const SectionLight = ({ children }) => <div className="homeSection">{children}</div>
 
-const SectionDark = ({ children }) => {
-
-  const { isDarkTheme } = useThemeContext();
-  const [backgroundColor, setBackgroundColor] = useState(isDarkTheme ? '#1c1d1f' : '#f4f5f7')
-
-  useEffect(() => {
-    setBackgroundColor(isDarkTheme ? '#1c1d1f' : '#f4f5f7')
-  }, [isDarkTheme]);
-
-  return <div style={{ backgroundColor: backgroundColor }} className="homeSection">{children}</div>
-}
+const SectionDark = ({ children }) => <div className="homeSection">{children}</div>
 
 export { SectionLight, SectionDark }
