77d5c9180f092becfddeccbc0a33ca43fbb1d6db
Vedant Shrotria <vedant.shrotria@harness.io>
2022-08-23 19:59:23 +0530

    Updated links for available tutorials (#202)
    * Added some minor changes for tutorials
    
    Signed-off-by: Vedant <vedant.srotria@harness.io>
    
    * Updated links for tutorials
    
    Signed-off-by: Vedant <vedant.srotria@harness.io>
    
    Signed-off-by: Vedant <vedant.srotria@harness.io>
    Co-authored-by: Vedant <vedant.srotria@harness.io>

--- website/src/components/section/Tutorials/data.js
+++ website/src/components/section/Tutorials/data.js
@@ -7,39 +7,38 @@ export const TutorialData = [
     link: 'https://litmuschaos.github.io/tutorials/tutorial-getting-started/index.html#0'
   },
   {
-    heading: 'Creating Custom Chaos Workflows',
-    description: 'Usage MyHub and Asset Discovery to create Custom Workflows',
+    heading: 'Creating Custom Chaos Scenarios',
+    description: 'Usage MyHub and Asset Discovery to create Custom Chaos Scenarios',
     image: '/img/Tutorials/custom-chaos.png',
-    isAvailable: false
+    isAvailable: true,
+    link: 'https://litmuschaos.github.io/tutorials/tutorial-create-custom-chaos-scenarios/index.html#0'
   },
   {
-    heading: 'Running Chaos Workflows On Remote Clusters',
-    description: 'Usage MyHub and Asset Discovery to create Custom Workflows',
+    heading: 'Running Chaos Scenarios On Remote Clusters',
+    description: 'External Chaos Delegate Set Up with LitmusCTL',
     image: '/img/Tutorials/remote-cluster.png',
-    isAvailable: false
+    isAvailable: true,
+    link: 'https://litmuschaos.github.io/tutorials/tutorial-running-chaos-scenario-on-remote-cluster/index.html#0'
   },
   {
     heading: 'Defining Steady-State Hypothesis',
     description: 'Usage of Litmus Probes to Create Constraints for Experiment Success',
     image: '/img/Tutorials/probes.png',
-    isAvailable: false
-  },
-  {
-    heading: 'Observing Chaos Impact On Services',
-    description: 'Instrument Grafana Dashboards with LitmusChaos Prometheus Metrics',
-    image: '/img/Tutorials/monitoring.png',
-    isAvailable: false
+    isAvailable: true,
+    link: 'https://litmuschaos.github.io/tutorials/tutorial-defining-steady-state-hypothesis/index.html#0'
   },
   {
     heading: 'Integrating with GitOps',
-    description: 'Usage of Git as Golden Copy for Chaos Workflows and Automated Trigger of Chaos',
+    description: 'Usage of Git as Golden Copy for Chaos Scenarios and Automated Trigger of Chaos',
     image: '/img/Tutorials/gitops.png',
-    isAvailable: false
+    isAvailable: true,
+    link: 'https://litmuschaos.github.io/tutorials/tutorial-integrating-gitops/index.html#0'
   },
   {
     heading: 'Teaming in LitmusChaos',
     description: 'Invite Team Members and Assign Roles in Chaos Project.',
     image: '/img/Tutorials/teaming.png',
-    isAvailable: false
+    isAvailable: true,
+    link: 'https://litmuschaos.github.io/tutorials/tutorials-teaming-in-litmus/index.html#0'
   }
 ]
