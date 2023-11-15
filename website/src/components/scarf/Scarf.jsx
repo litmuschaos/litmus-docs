d1e051b4b24ff09b3d5bbc5f150995c3cce5e90b
Hrishav <hrishav.kumar@harness.io>
2023-11-15 15:13:37 +0530

    chore: Added Scarf Tag For Tracking (#246)
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>
    Signed-off-by: SmritiSatya <smriti.satyanarayana@harness.io>

---
+++ website/src/components/scarf/Scarf.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ScarfTag = () => {
+  return (
+    <img
+      alt="scarf"
+      referrerPolicy="no-referrer-when-downgrade"
+      src="https://static.scarf.sh/a.png?x-pxid=a56ed60e-6f2d-465c-ab2a-409f2086abc5"
+    />
+  )
+}
+
+export { ScarfTag }
